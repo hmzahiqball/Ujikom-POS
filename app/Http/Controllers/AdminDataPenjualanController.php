7461db9596e2f3ceaa567a451c348e8c304918f7
Putra28 <suyapratama2217@gmail.com>
2025-04-17 10:30:38 +0700

    updated data penjualan

--- app/Http/Controllers/AdminDataPenjualanController.php
+++ app/Http/Controllers/AdminDataPenjualanController.php
@@ -4,18 +4,50 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Carbon\Carbon;
 
 class AdminDataPenjualanController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $id_petugas = null;
-        $get_kategori = DB::select('CALL sp_get_datakategori()'); //mengambil data kategori dari database melalui stored procedure di mysql
-        $get_penjualanperiode = DB::select('CALL sp_get_datatransaksi(?, ?)', [date('Y-m-d'), $id_petugas]);
-        return view('admin.datapenjualan' , ['penjualan' => $get_penjualanperiode, 'kategori' => $get_kategori]);
+        try {
+            // Ambil input tanggal, jika kosong maka set default awal dan akhir bulan ini
+            $tanggal = $request->input('tanggal');
+            if (!$tanggal) {
+                $startDate = Carbon::now()->startOfMonth()->format('Y-m-d');
+                $endDate = Carbon::now()->endOfMonth()->format('Y-m-d');
+                $tanggal = $startDate . '_' . $endDate;
+            }
+
+            // Tidak ada idpetugas, jadi biarkan null (jangan dikirim ke API)
+            $url = 'http://localhost:1111/api/laporanPenjualan';
+
+            $response = Http::get($url, [
+                'tanggal' => $tanggal
+            ]);
+
+            if ($response->successful()) {
+                $data_penjualan = $response->json('data');
+            } else {
+                $data_penjualan = [];
+            }
+
+            return view('admin.datapenjualan', [
+                'penjualan' => $data_penjualan,
+                'tanggal' => $tanggal,
+                'success' => $response['message']
+            ]);
+        } catch (\Exception $e) {
+            return view('admin.datapenjualan', [
+                'penjualan' => [],
+                'tanggal' => [],
+                'error' => 'Gagal mengambil data penjualan'
+            ]);
+        }
     }
 
     /**
